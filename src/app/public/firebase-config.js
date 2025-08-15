f661e7535db3f89886e6338636a9880b227dcc64
matheus nascimento <matheusnascimentorochaa@gmail.com>
2025-08-15 15:18:30 +0000

    configuração do firebase:
     apiKey: "AIzaSyD_Zk0kau4hmS9gWNxOTq77ZQSgOKn

--- src/app/public/firebase-config.js
+++ src/app/public/firebase-config.js
@@ -1,11 +1,9 @@
 const firebaseConfig = {
-  projectId: 'barberflow-vt26q',
-  appId: '1:1092239374925:web:e8ff3a5e79ab999e6bba04',
-  storageBucket: 'barberflow-vt26q.firebasestorage.app',
-  apiKey: 'AIzaSyDsw6ox_fmME37xw9qQhmv6MJW53CD7O68',
-  authDomain: 'barberflow-vt26q.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '1092239374925',
+  apiKey: "AIzaSyD_Zk0kau4hmS9gWNxOTq77ZQSgOKnjgqc",
+  authDomain: "barberflow-vt26q.firebaseapp.com",
+  projectId: "barberflow-vt26q",
+  storageBucket: "barberflow-vt26q.firebasestorage.app",
+  messagingSenderId: "1092239374925",
+  appId: "1:1092239374925:web:e8ff3a5e79ab999e6bba04"
 };
-
 export default firebaseConfig;
