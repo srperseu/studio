b23d958a036ace41956fd5982aa3bc03b3c8b684
matheus nascimento <matheusnascimentorochaa@gmail.com>
2025-08-15 14:51:38 +0000

    Deixa sem esse identitytool kit, vamos voltar pro que tava, nao ta funci

--- src/app/public/firebase-config.js
+++ src/app/public/firebase-config.js
@@ -1,8 +1,9 @@
+// IMPORTANT: This file is updated automatically by the Firebase Studio AI. Do not edit manually.
 const firebaseConfig = {
   "projectId": "barberflow-vt26q",
   "appId": "1:1092239374925:web:e8ff3a5e79ab999e6bba04",
   "storageBucket": "barberflow-vt26q.firebasestorage.app",
-  "apiKey": "AIzaSyDXGK_5PyQ0o1EAO3EWP8sdlNT6MmIJlGQ",
+  "apiKey": "AIzaSyC7LW0VRmJFQXbofAP8wpCeoMAnl_i6egE",
   "authDomain": "barberflow-vt26q.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1092239374925"
