cb648c4c0542c53c335f5fc91d502ff4e278fa80
matheus nascimento <matheusnascimentorochaa@gmail.com>
2025-08-15 14:34:56 +0000

    File changes

---
+++ src/app/public/firebase-config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: "barberflow-vt26q",
+  appId: "1:1092239374925:web:e8ff3a5e79ab999e6bba04",
+  storageBucket: "barberflow-vt26q.firebasestorage.app",
+  apiKey: "AIzaSyD_Zk0kau4hmS9gWNxOTq77ZQSgOKnjgqc",
+  authDomain: "barberflow-vt26q.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "1092239374925"
+};
+
+export default firebaseConfig;
