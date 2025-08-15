c72ee6ed91d1ea4238dc11e95b49148c1604d932
matheus nascimento <matheusnascimentorochaa@gmail.com>
2025-08-15 15:14:18 +0000

    Acho que voce configurou essa api identity errada, nao deve ta apontando

--- src/app/public/firebase-config.js
+++ src/app/public/firebase-config.js
@@ -1,11 +1,11 @@
-// IMPORTANT: This file is updated automatically by the Firebase Studio AI. Do not edit manually.
 const firebaseConfig = {
-  "projectId": "barberflow-vt26q",
-  "appId": "1:1092239374925:web:e8ff3a5e79ab999e6bba04",
-  "storageBucket": "barberflow-vt26q.firebasestorage.app",
-  "apiKey": "AIzaSyC7LW0VRmJFQXbofAP8wpCeoMAnl_i6egE",
-  "authDomain": "barberflow-vt26q.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1092239374925"
+  projectId: 'barberflow-vt26q',
+  appId: '1:1092239374925:web:e8ff3a5e79ab999e6bba04',
+  storageBucket: 'barberflow-vt26q.firebasestorage.app',
+  apiKey: 'AIzaSyDsw6ox_fmME37xw9qQhmv6MJW53CD7O68',
+  authDomain: 'barberflow-vt26q.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '1092239374925',
 };
+
 export default firebaseConfig;
