c8d1dcbd46e8183fe9e023423b9fdafb61503c6b
matheus nascimento <matheusnascimentorochaa@gmail.com>
2025-08-21 02:14:03 +0000

    Funcionou.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"Inter"', 'sans-serif'],
-        headline: ['"Oswald"', 'sans-serif'], // Trocamos para a Oswald
+        body: ['"Space Grotesk"', 'sans-serif'],
+        headline: ['"Space Grotesk"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
